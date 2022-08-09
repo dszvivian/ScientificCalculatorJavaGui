472f0cefef2d7a8c0ede31d1cb3f069c1c218ae1
Your Name <you@example.com>
2022-08-09 17:26:40 +0530

    Working

---
+++ src/main/java/com/mycompany/scalci/Scalci.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.scalci;
+
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.JFrame;
+
+
+public class Scalci{
+
+    public static void main(String[] args) {
+       new Calculator().setVisible(true);
+    }
+
+}
